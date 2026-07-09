5c52e9ceeeb782e77c4c4494a510f6cb71ccd7c9
imm95 <im95@gdrivepro.com>
2026-07-09 09:00:05 +0700

    fix: tampilkan nomor WA dan perbaiki warna ikon WhatsApp di halaman sukses booking

--- storage/framework/views/699dfb7ddc9eb60706fb992f8e2ab2f5.php
+++ storage/framework/views/699dfb7ddc9eb60706fb992f8e2ab2f5.php
@@ -32,4 +32,4 @@
         </div>
     </body>
 </html>
-<?php /**PATH C:\laragon\www\webembun\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions\views\minimal.blade.php ENDPATH**/ ?>
+<?php /**PATH C:\laragon\www\webembun\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/minimal.blade.php ENDPATH**/ ?>
--- storage/framework/views/bef2ec3da26abd3b7aeb00500c92ecb3.php
+++
@@ -1,3 +0,0 @@
-<?php echo value($html); ?>
-
-<?php /**PATH C:\laragon\www\webembun\vendor\filament\support\resources\views\anonymous-partial.blade.php ENDPATH**/ ?>
